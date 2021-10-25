3c62a280695f7442dad705ab3b7555420ef4bf44
mh031351 <mh031351@gmail.com>
2021-10-25 09:13:43 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,216 @@
+
+
+
+alert("INTRODUCTION")
+
+var username = prompt ("Enter your first name")
+
+// var lastName = prompt ("Enter your last name");
+
+var age = prompt ("age")
+
+// var subject = prompt ("subject");
+
+var math = prompt ("math")
+
+var urdu = prompt ("english")
+
+var islamiat = prompt ("islamiat")
+
+var sindhi = prompt ("sindhi")
+
+var totalmarks = prompt ("totalmarks")
+
+var obtainedmarks = prompt ("obtainedmarks")
+
+var totalpercentage = prompt ("totalpercentage")
+
+document.write(" <br> My Name : " + username ) 
+
+document.write(" <br>  Age :" + age)
+
+// document.write("All Subject" + subject);
+
+document.write(" <br> Math number : " + math)
+
+document.write(" <br> Urdu number : " + urdu)
+
+document.write(" <br> Islamiat Number : " + islamiat)
+
+document.write(" <br> Sindhi Number: " + sindhi)
+
+document.write(" <br> Total Number : 480" )
+
+document.write(" <br> Obtained Number: " + obtainedmarks )
+
+document.write(" <br> Total percentage :" + totalpercentage )
+
+
+// var name = ("username"); 
+
+alert("Complete Ceriataria")    
+
+// var name = prompt("Enter Your Name")
+
+// var roll = prompt("Enter your name");
+
+
+
+// var standard = prompt("Enter your class");
+
+
+// var math = prompt("Enter your math marks");
+
+
+// var urdu = prompt("Enter your urdu marks");
+
+
+// var physics = prompt("Enter your physics marks");
+
+
+// var chemistry = prompt("Enter your chemistry marks");
+
+// var english = prompt("Enter your english marks");
+
+// var obtained_marks = parseInt(math) + parseInt(urdu) + parseInt(physics) + parseInt(chemistry)  + parseInt(english);
+
+// var percentage = obtained_marks * 100 / 500;
+
+// d0cument.write("Your name is :" + name + "<br/>");
+
+// d0cument.write("Your Roll Number :" + roll + "<br/>");
+
+// d0cument.write("Your Class is :" + standard + "<br/>");
+
+// d0cument.write("Your Total marks:" + obtained_marks + "<br/>");
+
+// d0cument.write("Your Percentage is :" + percentage + "<br/>");
+
+
+
+
+///////////////veriable/////////////
+
+// There are two type of veriable
+
+// 01-leagle  
+
+// 02-Inleagle     
+
+// advantages & Disadvantage
+
+// Dont uses for this  space
+
+// Alphabats ma double cotation laga lazmi hota hay
+
+// Number ma double cotation ham (nahi) lagata hay
+
+// Aur ($) dollor lagakay bhi koi kam kar sakhtay hay
+
+
+// Please all of student use of (CAMEL CASE)
+
+// Example: firstName . lastName . userName  in may (N) pay ghor kary
+
+// ///////////////Case sensitive/////////
+
+// First horf CAPITAL ma lakhna hay
+
+// Example: Name . Math .Urdu   in may first horf pay ghor kary
+
+
+
+
+//////////////veriable Name///////
+
+
+// Veriable defination:
+
+// Hamari memory ka andar ak space (reserve) karta hay
+
+// var                               key word
+
+// name                              inilization
+
+// =                                 sing operater
+
+// ;                                 statement terminater
+
+// 120                               variable  value
+
+
+//                /////// Data Types////////
+
+
+// String Data means ("Alphabats")
+
+
+// Number Data means ("1,2,3,4,5....")
+
+
+// Booleans  means ("True +  False")
+
+
+// Undefined
+
+
+// Null
+
+
+
+
+               ///////Operater////////
+
+
+// +   addition
+
+// -   subtraction
+
+// *   multiplycation
+
+// /   division
+
+// %   percentage
+
+
+
+            ////Frand Hand/////
+
+// Frand hand uses of (client site)
+
+//         //////Back Hand/////////
+
+// Back hand uses of (server)
+
+
+//         //////node js//////
+
+// Node js server ka lia uses hota hay
+
+
+    ///////////////Javascript language////////////
+
+// scripting language
+
+// programming language
+
+// lozly  type language
+
+// object base programming language .......
+
+
+//////////Translater///////////
+
+// There are three typs of translater.
+
+// 01-Interpetion uess of (left to right line by line)
+
+// 02-Complied  uses of (pora code ak sath read karta hay)
+
+// 03-Asmylor  uses of  (Mother board waghra kay lia use hota hay)
+
+//             (javascript left to right chalthi hay)
+
+
+
+
